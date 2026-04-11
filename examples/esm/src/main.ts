8e492732ac62ad5f18c4531a4dfe32e46030d19b
Stephen Hellicar <shellicar@gmail.com>
2026-04-12 04:03:39 +1000

    Archive notice: package moved to shellicar/ecosystem

--- examples/esm/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createCosmosQueryBuilder, SortDirection } from '@shellicar/cosmos-query-builder';
-
-type Person = {
-  type: 'Person';
-  age: number;
-};
-
-const builder = createCosmosQueryBuilder<Person>();
-builder.orderBy('age', SortDirection.Desc);
--- packages/cosmos-query-builder/src/private/consts.ts
+++
@@ -1,8 +0,0 @@
-export const operators: Record<string, string | undefined> = {
-  eq: '=',
-  ne: '!=',
-  ge: '>=',
-  gt: '>',
-  le: '<=',
-  lt: '<',
-} as const;
